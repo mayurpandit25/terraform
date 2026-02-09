d6c7d7c841c97fe6613130be73c3d6065fc84875
mayurpandit25 <panditmayur888@gmail.com>
2026-02-09 14:41:07 +0530

    install_apache.sh

--- install_apache.sh
+++ install_apache.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt install apache2 -y
-sudo systemctl start apache2
-echo "<h1> Apache2 has been installed and started successfully </h1>" > /var/www/html/index.html
-sudo systemctl restart apache2
-sudo systemctl enable apache2 
+set -xe
+
+apt-get update -y
+apt-get install -y apache2
+
+systemctl start apache2
+systemctl enable apache2
+
+echo "<h1>Apache2 has been installed and started successfully</h1>" > /var/www/html/index.html
+systemctl restart apache2
