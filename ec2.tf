e5e7b27aee72ed0410b029e389503e3e5f8ac1fb
mayurpandit25 <panditmayur888@gmail.com>
2026-03-13 16:23:53 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,14 @@
 resource "aws_instance" "public_instance" {
+    for_each = tomap({
+        server1 = "t3.micro"
+        server2 = "t3.medium"
+    })
+
     ami = "ami-019715e0d74f695be"
-    instance_type = "t3.micro"
+    instance_type = each.value
     key_name = "id_rsa"
     vpc_security_group_ids = [aws_security_group.sg.id]
     user_data = file("/root/terraform/user_data.sh")
-    count = 2
 
     root_block_device {
       volume_size = 10
@@ -12,6 +16,6 @@ resource "aws_instance" "public_instance" {
     }
 
     tags = {
-        Name = "ubuntu"
+        Name = each.key
     } 
 }
