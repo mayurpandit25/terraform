7f43579ed5821ed47eddedc86b81cbf955f397a4
mayurpandit25 <panditmayur888@gmail.com>
2026-03-13 16:12:10 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -3,14 +3,7 @@ resource "aws_instance" "public_instance" {
     instance_type = "t3.micro"
     key_name = "id_rsa"
     vpc_security_group_ids = [aws_security_group.sg.id]
-    user_data=<<-EOF
-              #!/bin/bash
-              sudo apt update 
-              sudo apt install apache2 -y
-              sudo systemctl start apache2
-              sudo systemctl enable apache2
-              sudo echo "<h1> Welcome to ubuntu </h1>" | tee /var/www/html/index.html
-              EOF
+    user_data = file("/root/terraform/user_data.sh")
     tags = {
         Name = "ubuntu"
     } 
