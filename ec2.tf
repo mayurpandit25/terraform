ac4a0db081a059aa444487646f8efc79db0dfa5e
mayurpandit25 <panditmayur888@gmail.com>
2026-03-13 16:18:45 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -4,6 +4,13 @@ resource "aws_instance" "public_instance" {
     key_name = "id_rsa"
     vpc_security_group_ids = [aws_security_group.sg.id]
     user_data = file("/root/terraform/user_data.sh")
+    count = 2
+
+    root_block_device {
+      volume_size = 10
+      volume_type = "gp3"
+    }
+
     tags = {
         Name = "ubuntu"
     } 
