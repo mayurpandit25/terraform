87b8f115ca6905def57b62b81c82fddd8af2f862
mayurpandit25 <panditmayur888@gmail.com>
2026-03-13 14:22:01 +0530

    terraform

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "public_instance" {
     ami = "ami-019715e0d74f695be"
     instance_type = "t3.micro"
     key_name = "id_rsa"
+    vpc_security_group_ids = [aws_security_group.sg]
     tags = {
         Name = "ubuntu"
     } 
