83d206d39039d3f65fea4748ef35210f12df2c2d
mayurpandit25 <panditmayur888@gmail.com>
2026-03-13 13:05:13 +0530

    terraform

---
+++ ec2.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "public_instance" {
+    ami = "ami-019715e0d74f695be"
+    instance_type = "t3.micro"
+    key_name = "ubuntu1"
+    tags = {
+        Name = "ubuntu"
+    } 
+}
