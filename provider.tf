5fbc77235858dd6163d25a687100e55c3a023f66
mayurpandit25 <panditmayur888@gmail.com>
2026-03-15 19:19:57 +0530

    provider

--- provider.tf
+++ provider.tf
@@ -2,3 +2,11 @@ provider "aws" {
     region = "ap-south-1"
     profile = "config"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "amazon-s3-bucket-mayur121"
+    key = "terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
