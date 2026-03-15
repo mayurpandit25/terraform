16f78a605b60554609bed71a27f27088238800df
mayurpandit25 <panditmayur888@gmail.com>
2026-03-15 19:31:51 +0530

    provider

--- provider.tf
+++ provider.tf
@@ -8,5 +8,8 @@ terraform {
     bucket = "amazon-s3-bucket-mayur121"
     key = "terraform.tfstate"
     region = "ap-south-1"
+    profile = "configs"
+    shared_credentials_files = [ "/root/.aws/credentials" ]
+    dynamodb_table = "cbz"
   }
 }
