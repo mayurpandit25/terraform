57a1011267a92b6c5d9313bf8945f69bf329f51b
mayurpandit25 <panditmayur888@gmail.com>
2026-03-15 14:47:18 +0530

    module

--- module/vpc/output.tf
+++ module/vpc/output.tf
@@ -10,13 +10,6 @@ output "private_subnet_id" {
     value = aws_subnet.private_subnet.id
 }
 
-output "public_ip" {
-    value = aws_instance.public_instance.public_ip
-}
-
-output "private_ip" {
-    value = aws_instance.private_instance.private_ip
-}
 
 
 
