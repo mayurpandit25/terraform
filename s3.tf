c579ba4cc802683db10ed479e0d959a3c7ef74e0
Mayur Pandit <130488383+mayurpandit25@users.noreply.github.com>
2026-04-01 16:03:24 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,4 +1,4 @@
-/* resource "aws_s3_bucket" "s3_bucket" {
+resource "aws_s3_bucket" "s3_bucket" {
     region = "ap-south-1"
     bucket = "mayur-s3-bucket-amazon1232"
 
@@ -10,7 +10,12 @@
         Name = "mayur-s3-bucket-amazon1232"
     }
 }
- */
+resource "aws_s3_object" "file_upload" {
+  bucket = aws_s3_bucket.s3_bucket.bucket
+  source = "/root/terraform/file.txt"
+  key    = "file.txt"
+  acl    = "private"
+}
 
 /* resource "random_id" "rand_id" {
   byte_length = 8
